89d04bbdfaf343a40bf854b03751df0776001aab
John Major <john@orbonline.co.uk>
2018-05-08 12:37:11 +0100

    Forced Settings now work! Code cleaned a bit also. Now need to start adding visual representation of how far along game is

--- resources/js/setUp.js
+++ resources/js/setUp.js
@@ -2,12 +2,11 @@
 // add Item button can add children
 addItemBtn.addEventListener("click", addItem);
 // getting team names as long as more than 2 are created
-readyBtn.addEventListener("click", grabTeamNames);
+readyBtn.addEventListener("click", showSettings);
 // add ruleBtn event listener
 rulesBtn.addEventListener('click', showRules);
 // close rules btn event addEventListener
 document.getElementById('closeRules').addEventListener('click', hideRules);
-
 // Add Teams and players
 function addItem() {
   noOfTeams += 1;
@@ -28,7 +27,6 @@ function addItem() {
   document.getElementById(`addPlayer_${randNumId}`).addEventListener('click', addPlayer);
   nextArrow.addEventListener('click', rulesContentChangeNo2);
 }
-
 function addPlayer() {
   noOfPlayers += 1;
   var id = this.id;
@@ -44,10 +42,8 @@ function addPlayer() {
   this.innerHTML = 'Delete Third Player';
   this.addEventListener('click', deleteThirdPlayer);
 }
-
 function deleteThirdPlayer() {
   noOfPlayers -=1;
-
   var identity = this.id;
   var identityNos = identity.substring(10, this.id.length);
   var player3Identity = 'player3Id' + identityNos;
@@ -58,107 +54,67 @@ function deleteThirdPlayer() {
   this.addEventListener('click', addPlayer);
   this.innerHTML = 'Add Third Player';
 }
-
 function deleteItem() {
   var item = this.parentNode;
   document.getElementById('list').removeChild(item);
   noOfTeams -= 1;
 }
-
-function grabTeamNames() {
-  // Honey Pot
+function hideSettings() {
+  document.getElementById('settings-modal').className = 'hidden';
+  document.getElementById('settings-container').className = 'hidden';
+}
+function showSettings() {
+  // Check if it's ok to make everything appear
+    // 1. Honey Pot
   if(document.getElementById('computer').value != '') {
     alert('something\'s not quite right');
     return;
-  } else {
-      var teamNames = document.querySelectorAll('.teamNames');
-      var playerNames = document.querySelectorAll('.playerNames');
-
-      if(noOfTeams < 2) {
-        alert("You must have at least two teams");          // CHANGE THIS TO SOMETHING GOOD
+  }
+    // 2. Check correct number of teams
+    if(noOfTeams < 2) {
+      alert("You must have at least two teams");          // CHANGE THIS TO SOMETHING GOOD
+      return;
+    }
+    // 3. Check Teams have a value entered
+    var teamNames = document.querySelectorAll('.teamNames');
+    for(i = 0; i < teamNames.length; i++) {
+      if(teamNames[i].value.length < 2) {
+        alert('please enter a valid team names in all boxes provided'); // CHANGE THIS TO SOMETHING GOOD
         return;
-      } else {
-     //create arrays of team names and player names
-              for(i=0; i < teamNames.length; i++) {
-                  teamNamesArray.push(teamNames[i].value);
-                  // If no values are entered, get rid of that shit
-                  if(teamNames[i].value == '') {
-                    alert('Please enter your team names');        // CHANGE THIS TO SOMETHING GOOD
-                    for(let y=0; y < teamNames.length; y++) {
-                      teamNamesArray.pop();
-                    }
-                    for(let y=0; y < playerNames.length; y++) {
-                      playerNamesArray.pop();
-                      return;
-                        }
-                    }
-        }
-    // loop through playerNamesArray to form new array
-    for(i=0; i < playerNames.length; i++) {
-      playerNamesArray.push(playerNames[i].value);
-      if(playerNames[i].value == '') {
-        alert('Please enter your Player names');
-        for(let y=0; y < teamNames.length; y++) {
-          teamNamesArray.pop();
-        }
-        for(let y=0; y < playerNames.length; y++) {
-          PlayerNamesArray.pop();
-          return;
-        }
       }
     }
-  }
-// create team Objects
-    for(i=0; i < noOfTeams; i++) {
-      var numberOfPlayers = list.children[i].querySelector('div').children.length;
-      var specificTeamPlayerNames = [];
-
-        for(var k=numberOfPlayers - 1; k >= 0; k--) {
-          specificTeamPlayerNames.push(playerNamesArray[k]);
-          playerNamesArray.splice(k, 1);
-        }
-
-        teamObjectsArray[i] = {
-          name: teamNamesArray[i],
-          players: specificTeamPlayerNames,
-          score: 0,
-          roundsPlayed: 0,
-          passesUsed: 0,
-          position: 1,
-          whichPlayersTurn: 0,
-        };
+    // 4. Check playerNames all have a value
+    var playerNames = document.querySelectorAll('.playerNames');
+    for(i = 0; i < playerNames.length; i++) {
+      if(playerNames[i].value.length < 2) {
+        alert('please enter valid player names in all boxes provided'); // CHANGE THIS TO SOMETHING GOOD
+        return;
       }
-
     }
-  while(list.firstChild) {
-    list.removeChild(list.firstChild);
-  }
-  addItemBtn.className = 'hidden';
-  readyBtn.removeEventListener("click", grabTeamNames);
-// SETTINGS BASICALLY
-function showSettings() {
-   document.getElementById('settings-modal').className = 'initialSettings';
-   document.getElementById('settings-container').className = 'initialSettings';
-  setTimeout(function(){
-    document.getElementById('settings-modal').className = 'settings-modal';
-    document.getElementById('settings-container').className = 'modal-container';
-  }, 1);
-  let zero = 0;
-  let one = 1;
-  let two = 2;
-  let three = 3;
-  let four = 4;
-  let five = 5;
+  // Make everything appear
+ document.getElementById('settings-modal').className = 'initialSettings';
+ document.getElementById('settings-container').className = 'initialSettings';
+ // slight delay to create fade in
+setTimeout(function(){
+  document.getElementById('settings-modal').className = 'settings-modal';
+  document.getElementById('settings-container').className = 'modal-container';
+}, 1);
+    let zero = 0;
+    let one = 1;
+    let two = 2;
+    let three = 3;
+    let four = 4;
+    let five = 5;
 
-  let twenty = 20;
-  let forty = 40;
-  let sixty = 60;
-  let eighty = 80;
-  let hundred = 100;
-  let twohundred = 200;
+    let twenty = 20;
+    let forty = 40;
+    let sixty = 60;
+    let eighty = 80;
+    let hundred = 100;
+    let twohundred = 200;
 
-  let thirty = 30;
-  let fortyfive = 45;
+    let thirty = 30;
+    let fortyfive = 45;
   let settingsHTML = `<h1>Settings</h1>
                       <div class='settings-row'>
                         <span>Points needed to win</span>
@@ -198,49 +154,66 @@ function showSettings() {
                           <option value='Disabled'>Disabled</option>
                         </select>
                       </div>`;
-
+// Change relevant button functions
   document.getElementById('settingsContent').innerHTML = settingsHTML;
-  document.getElementById('closeSettings').addEventListener('click', backToTeamMaker);
+  document.getElementById('closeSettings').addEventListener('click', hideSettings);
   document.getElementById('saveSettings').addEventListener('click', saveSettings);
-
 }
-showSettings();
 function saveSettings() {
-  // grab values
-  let pointsToWinSelect = document.getElementById('pointsToWinSelect').value;
-  let timerSelect = document.getElementById('timerSelect').value;
-  let passesSelect = document.getElementById('passesSelect').value;
-  let drinkingSelect = document.getElementById('drinkingSelect').value;
-  // edit global variables
-  timer = timerSelect;
-  toWin = pointsToWinSelect;
-  maximumPasses = passesSelect;
-
-  if(drinkingSelect == 'Enabled') {
-    drinkRules = true;
-  } else {
-    drinkRules = false;
+  // Gather Team and Player Names in an array
+  var teamNames = document.querySelectorAll('.teamNames');
+  var playerNames = document.querySelectorAll('.playerNames');
+  for(i = 0; i < teamNames.length; i++) {
+    teamNamesArray.push(teamNames[i].value);
   }
-  function startDaGame() {
-    // Ensure first team that starts is random
-  let randomStart = Math.floor(Math.random() * noOfTeams);
-  whichTeamPlays += randomStart;
-  roundPrep(teamObjectsArray[whichTeamPlays%teamObjectsArray.length]);
-    }
-    hideSettings();
-    startDaGame();
-}
-function hideSettings() {
-  document.getElementById('settings-modal').className = 'hidden';
-  document.getElementById('settings-container').className = 'hidden';
-}
-// So that too many teams don't get accidentally added to the arrays etc
-  function backToTeamMaker() {
-    noOfPlayers -= noOfPlayers;
-    teamObjectsArray.length = 0;
-
-    document.getElementById('settings-modal').className = 'hidden';
-    document.getElementById('settings-container').className = 'hidden';
-
-    }
+  for(i = 0; i < playerNames.length; i++) {
+    playerNamesArray.push(playerNames[i].value);
   }
+        // grab values
+        let pointsToWinSelect = document.getElementById('pointsToWinSelect').value;
+        let timerSelect = document.getElementById('timerSelect').value;
+        let passesSelect = document.getElementById('passesSelect').value;
+        let drinkingSelect = document.getElementById('drinkingSelect').value;
+        // edit global variables
+        timer = timerSelect;
+        toWin = pointsToWinSelect;
+        maximumPasses = passesSelect;
+        // Check if drinking rules should be enabled
+        if(drinkingSelect == 'Enabled') {
+          drinkRules = true;
+        } else {
+          drinkRules = false;
+        }
+        // create team Objects
+            for(i=0; i < noOfTeams; i++) {
+              var numberOfPlayers = list.children[i].querySelector('div').children.length;
+              var specificTeamPlayerNames = [];
+              for(var k=numberOfPlayers - 1; k >= 0; k--) {
+                specificTeamPlayerNames.push(playerNamesArray[k]);
+                playerNamesArray.splice(k, 1);
+              }
+                teamObjectsArray[i] = {
+                  name: teamNamesArray[i],
+                  players: specificTeamPlayerNames,
+                  score: 0,
+                  roundsPlayed: 0,
+                  passesUsed: 0,
+                  position: 1,
+                  whichPlayersTurn: 0,
+                };
+              }
+        function startDaGame() {
+          // Ensure first team that starts is random
+        let randomStart = Math.floor(Math.random() * noOfTeams);
+        whichTeamPlays += randomStart;
+        roundPrep(teamObjectsArray[whichTeamPlays%teamObjectsArray.length]);
+          }
+          hideSettings();
+          // Get rid of what's left on the screen and no longer needed event listeners
+          while(list.firstChild) {
+            list.removeChild(list.firstChild);
+          }
+          addItemBtn.className = 'hidden';
+          readyBtn.removeEventListener("click", showSettings);
+          startDaGame();
+}
